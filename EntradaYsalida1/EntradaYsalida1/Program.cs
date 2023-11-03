f4fdbad269937d4769d90c401e97e9d9c8060d17
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 17:31:03 -0300

    Trabajando con la Libreria Sistem IO input ouput
    trabajando con archivos planos

---
+++ EntradaYsalida1/EntradaYsalida1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EntradaYsalida1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter otexto = new StreamWriter("prueba.txt");//sobre escribe o crea;
+           
+            otexto.WriteLine("Programador developer");
+            otexto.WriteLine("ariel");
+            otexto.Close();//cierra el archivo
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
