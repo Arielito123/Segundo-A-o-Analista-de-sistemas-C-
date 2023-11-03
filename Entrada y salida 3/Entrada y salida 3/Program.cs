0d66c0c85dcce9d5ad17193d71bfcb1e06f593a4
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 17:33:35 -0300

    seguimos Practicando con system io

---
+++ Entrada y salida 3/Entrada y salida 3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Entrada_y_salida_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter otexto = File.AppendText("Prueba.txt");
+            otexto.WriteLine("Hola Sistemas");
+            otexto.WriteLine("¿Como estan?");
+            otexto.Close();
+
+            TextReader otexto1 = new StreamReader ("Prueba.txt");
+            Console.WriteLine(otexto1.ReadToEnd());
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
