9f4232f335498a894360d2947ff68a2eca50a509
Arielito123 <104540300+Arielito123@users.noreply.github.com>
2023-11-03 17:21:26 -0300

    Producto stock con clases
    paradigma orientado a objectos

---
+++ ProductoStockClass/ProductoStockClass/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductoStockClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region objetos/variables
+            const int longitud=3;
+            int [] codigo=new int[longitud];
+            int[] cantidad = new int[longitud];
+            int[] generar = new int[longitud];
+            #endregion
+
+            #region cargaInformacion
+            for (int i = 0; i < codigo.Length; i++) 
+            {
+                Console.WriteLine("Ingrese el codigo de barra del producto");
+                codigo[i]  = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Ingrese la cantidad de  producto");
+                cantidad[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            #endregion
+            
+            #region instanciar clase
+            producto Oproducto = new producto();
+            Oproducto.Codigobarra = codigo;
+            Oproducto.Cantidad_disponible = cantidad;
+            #endregion
+           
+            #region parteBusqueda
+            Console.WriteLine("Ingrese un producto a buscar");
+            int buscar = Convert.ToInt32(Console.ReadLine());
+            int pos = 0;
+            while (buscar != 0) 
+            {
+                 pos = Oproducto.buscar(buscar);
+                
+                if (pos == -1)
+                {
+                    Console.WriteLine("No ha sido encontrado");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Ingrese la cantidad que deseas comprar" + " " + Oproducto.Cantidad_disponible[pos]);
+                    int cantidadCompra = Convert.ToInt32(Console.ReadLine());
+
+
+                    if (cantidadCompra > Oproducto.Cantidad_disponible[pos])
+                    {
+                        Console.WriteLine("No se puede hacer esa comprar supera el limite");
+                    }
+                    else 
+                    {
+                        Oproducto.Cantidad_disponible[pos] = Oproducto.Cantidad_disponible[pos] - cantidadCompra;
+                    }
+                }
+
+                 Console.WriteLine("Ingrese otro producto a buscar");
+                 buscar = Convert.ToInt32(Console.ReadLine());
+            }
+            #endregion
+
+            #region ParteGenerar
+            int  gen = Oproducto.generar(generar);
+           Console.WriteLine("-------------------------------------------------------------------------");
+           Console.WriteLine("Elementos con menos de 10 unidades");
+            
+            for (int i = 0; i < gen; i++) 
+
+            {
+                Console.WriteLine(" Codigo Barra: "+generar[i] + " " + "stock:"+ Oproducto.Cantidad_disponible[i]);
+            }
+            Console.WriteLine("cantidad de elementos generados con menos de 10 unidades"+gen);
+            #endregion
+
+            #region ParteOrdenar
+            Oproducto.ordernar();
+            Console.WriteLine("----------------------------------------------------------------------");
+            Console.WriteLine("Elemento ordenados por codigo de barra arrastrando el stock");
+
+            for (int i = 0; i < Oproducto.Codigobarra.Length; i++) 
+            {
+                Console.WriteLine(Oproducto.Codigobarra[i] + " " + Oproducto.Cantidad_disponible[i]);
+            }
+            #endregion
+            
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ ProductoStockClass/ProductoStockClass/producto.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductoStockClass
+{
+    class producto
+    {
+        #region carga
+        private Int32[] codigobarra;
+
+
+        private Int32[] cantidad_disponible;
+
+        #endregion
+
+        #region encapsulado
+        public Int32[] Codigobarra
+        {
+            get { return codigobarra; }
+            set { codigobarra = value; }
+        }
+
+
+        public Int32[] Cantidad_disponible
+        {
+            get { return cantidad_disponible; }
+            set { cantidad_disponible = value; }
+        }
+        #endregion
+
+        #region constructor
+
+        public producto() 
+        { 
+        }
+
+        #endregion
+
+
+
+        public Int32 buscar(int x) 
+        {
+            int k=0, i=0;
+
+            while (k == 0 && i < Codigobarra.Length)
+            {
+                if (x == Codigobarra[i])
+                {
+                    k = 1;
+                }
+
+                else 
+                {
+                    i++;
+                }
+            }
+
+            if (k == 0) 
+            {
+                i = -1;
+            }
+
+            return i;
+        }
+
+
+        public void ordernar()
+        {
+            int k = 0, i = 0,x=3,auxc=0,auxca=0;
+
+            
+            
+                while (k == 0) 
+                {
+                    k = 1;
+                    x--;
+
+                    for (i = 0; i < x; i++) 
+                    {
+                        if (Codigobarra[i] > Codigobarra[i + 1]) 
+                        {
+                            k = 0;
+                            auxc = Codigobarra[i];
+                            Codigobarra[i] = Codigobarra[i+1];
+                            Codigobarra[i+1] = auxc;
+                            
+                            auxca = Cantidad_disponible[i];
+                            Cantidad_disponible[i] = Cantidad_disponible[i + 1];
+                            Cantidad_disponible[i + 1] = auxca;
+
+
+
+                        }
+                    }
+                }
+            
+       
+           
+
+        
+
+        
+        
+        
+
+    }
+        
+        public int generar(int[] vectorgenerado)
+        {
+            int i=0,j=0;
+
+            for (i = 0; i < Codigobarra.Length; i++) 
+            {
+                if (Cantidad_disponible[i] < 10) 
+                {
+                    vectorgenerado[j] = Codigobarra[i];
+                    j++;
+                }
+            }
+            return j;
+        }
+    }
+}
